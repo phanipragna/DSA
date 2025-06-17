c46e06103b4b22dd31ab1ac908ef01fc10fc40bf
phanipragna <phanipragnachekuri@gmail.com>
2025-06-17 14:45:01 +0530

    Time: 1 ms (99.59%) | Memory: 57.3 MB (21.46%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -5,7 +5,8 @@ class Solution {
         for(int i=1;i<n;i++){
             if(csum<0){csum=0;}
             csum+=nums[i];
-            maxsum=Math.max(maxsum,csum);//if(maxsum<=csum){maxsum=csum;}
+            //maxsum=Math.max(maxsum,csum);
+            if(maxsum<=csum){maxsum=csum;}
         }return maxsum;
     }
 }
