f05cf874a68940b060afccd6690b7d69606e4a87
phanipragna <phanipragnachekuri@gmail.com>
2025-06-21 10:35:52 +0530

    Time: 4 ms (91.11%) | Memory: 58 MB (42.78%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -3,12 +3,8 @@ class Solution {
         int l=0,r=height.length-1,maxarea=0,area;
         while(l<r){
             if(height[l]<=height[r]){
-                area=height[l]*(r-l);
-                l++;}
-            else{
-                area=height[r]*(r-l);
-                r--;}
+                area=height[l]*(r-l);l++;}
+            else{area=height[r]*(r-l);r--;}
             maxarea=Math.max(maxarea,area);}
-        return maxarea;  
-    }
+        return maxarea;}
 }
