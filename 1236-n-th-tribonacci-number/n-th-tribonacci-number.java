d76bbcab19544c1901d61060b9da280a8a98aabe
phanipragna <phanipragnachekuri@gmail.com>
2025-06-24 15:54:01 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (50.17%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -1,6 +1,6 @@
 class Solution {
     public int tribonacci(int n) {
-        int a[]=new int[40],x=0;
+        int a[]=new int[38],x=0;
          a[0]=0;a[1]=1;a[2]=1;
         if (n == 0) return 0;
         if (n == 1 || n == 2) return 1;
