85bab61d0aba184ca6936b788303bceb7d4904f4
phanipragna <phanipragnachekuri@gmail.com>
2025-06-24 14:40:45 +0530

    Time: 5 ms (23.99%) | Memory: 47 MB (20.08%) - LeetSync

---
+++ 338-counting-bits/counting-bits.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] countBits(int n) {
+        int res[]=new int[n+1],j=0,c,t;
+        for(int i=0;i<=n;i++){
+        //method to get set bit
+         c=0;
+         t=i;
+        while(t>0){
+            if((t&1)==1)//if(i%2==1) 
+            c++;
+            t=t>>1;//i=i/2;
+        }
+        res[j]=c;
+        j++;
+    }return res;
+    }
+}
